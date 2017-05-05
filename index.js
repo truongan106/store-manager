a8d730a5ded2b15df6cc8bc32630c60c22f5fb74
truongan106 <truongan106@gmail.com>
2017-05-05 11:42:36 +0700

    add pug temp

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app=express();
 app.use(express.static('htdoc'));
 app.use('/components',express.static('bower_components'));
 
-app.get('/', function(req,res){
-  res.send('Hello');
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/pug', function(req,res){
+  res.render('index', {title:'Hey', message:'Hello'});
 });
 
 var port = 3400;

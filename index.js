c17a4dcb94d977320ac21c84ee70209fd14f146b
truongan106 <truongan106@gmail.com>
2017-05-05 11:03:26 +0700

    Add bower

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello a");
+var express = require('express');
+var app=express();
+
+app.use(express.static('htdoc'));
+app.use('/components',express.static('bower_components'));
+
+app.get('/', function(req,res){
+  res.send('Hello');
+});
+
+var port = 3400;
+app.listen(port, function(){
+console.log('Started Server');
+})

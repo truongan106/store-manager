f43ff98a6bfb225d015bb5d407f6615e89e59e59
truongan106 <truongan106@gmail.com>
2017-05-05 12:44:11 +0700

    add handlebars templates

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var app=express();
+var exphbs = require('express-handlebars');
 
-app.use(express.static('htdoc'));
+
 app.use('/components',express.static('bower_components'));
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
 
-app.get('/pug', function(req,res){
-  res.render('index', {title:'Hey', message:'Hello'});
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title:'Hey',
+    message:'Hello',
+    layout: 'application'
+  });
 });
 
 var port = 3400;
